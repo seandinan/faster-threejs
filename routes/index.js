2e54d6789ac97783b633f4566a3c76f510edfe3e
Sean Dinan <seancdinan@gmail.com>
2016-07-08 08:03:22 +0200

    Under current setup, I'm getting the problem that the POST request throws a 413 error, meaning that the request entity is too large

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index');
 });
+
+router.post('/modelThings', function(req, res){
+	console.log(req);
+	res.status(200).send('OK');
+})
 
 module.exports = router;
