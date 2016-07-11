4cae5cbb0a011383c6828262a2220bc249d89614
Sean Dinan <seancdinan@gmail.com>
2016-07-11 16:54:28 +0200

    Trying various things, forgot to commit for a while. BSON doesn't look too helpful. 58000000 length UInt8Array...

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -6,6 +6,7 @@ var camera   = new THREE.PerspectiveCamera(25, canvas.clientWidth / canvas.clien
 camera.position.z = 15;
 var renderer = new THREE.WebGLRenderer({canvas: canvas, alpha: true, precision: 'lowp'});
 renderer.setClearColor(0xF3F3F4);
+var newModel;
 // -----------------------------------------------------------------------------------
 var loader = new THREE.PLYLoader();
 // loader.load('/resources/monkey.ply', function(geometry){
@@ -18,22 +19,31 @@ loader.load('/resources/20160127-111211-FinalHighColorMesh.ply', function(geomet
 		side: THREE.DoubleSide
 	});
 	var teeth = new THREE.Mesh(geometry, material);
-	// teeth.scale.set(.08, .08, 0.08);
-	teeth.scale.set(1, 1, 1);
+	teeth.scale.set(.08, .08, 0.08);
+	// teeth.scale.set(1, 1, 1);
 	teeth.rotation.x = 3 * Math.PI / 2;
 	teeth.rotation.z = - Math.PI / 2;
 	scene.add(teeth);
+	var colors = JSON.stringify(teeth.geometry.colors);
+	var faces = JSON.stringify(teeth.geometry.faces);
+	var vertices = JSON.stringify(teeth.geometry.vertices);
+	var p = document.createElement('p');
+	var BSON = bson().BSON;
+	var bsonText = BSON.serialize(teeth.geometry);
+	console.log(bsonText);
+	document.body.removeChild(canvas);
 	sendModelData();
 });	
 // -----------------------------------------------------------------------------------
 function sendModelData(){
-	console.log('making model data...');
+	console.log(scene.children[0]);
+	// scene.children[0].toJSON = function(){return this};
 	var xhr = new XMLHttpRequest();
 	xhr.open('POST', 'modelThings', true);
 	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 	xhr.onreadystatechange = function(event){
 		if (xhr.readyState == xhr.DONE && xhr.status == 200){
-			console.log(JSON.parse(JSON.parse(event.target.responseText).modelData));
+			newModel = JSON.parse(JSON.parse(event.target.responseText).modelData);
 		}
 	}
 	xhr.send('modelData=' + JSON.stringify(scene.children[0]));
---
+++ public/scripts/objectCreation.js
@@ -0,0 +1,23 @@
+// -----------------------------------------------------------------------------------
+function buildNewModel(info){
+	var material = new THREE.MeshBasicMaterial({
+		color: 0xffffff,
+		vertexColors: THREE.VertexColors,
+		side: THREE.DoubleSide
+	});
+
+	// Assemble the geometry
+	var geometry = new THREE.Geometry();
+	
+
+
+
+
+
+	var model = new THREE.Mesh(geometry, material);
+};
+
+var info = {
+	colors:
+
+}
